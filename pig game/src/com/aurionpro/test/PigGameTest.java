9140ac7da0d9b5c2d891aa109c11e725abce6695
hemant878 <73839069+hemant878@users.noreply.github.com>
2023-07-18 22:04:02 +0530

    Add files via upload

---
+++ pig game/src/com/aurionpro/test/PigGameTest.java
@@ -0,0 +1,12 @@
+package com.aurionpro.test;
+
+import com.aurionpro.model.PigGame2;
+
+public class PigGameTest {
+
+	public static void main(String[] args) {
+		PigGame2 game = new PigGame2();
+        game.play();
+
+	}
+}
